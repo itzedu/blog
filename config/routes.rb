00aaf7ca4c018353e41cc4eb31390894958dd929
Eduardo Baik <edubaik@gmail.com>
2014-05-30 22:24:43 -0700

    Added comments and security

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blog::Application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   
   root "welcome#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530232211) do
+ActiveRecord::Schema.define(version: 20140531050136) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20140530232211) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
